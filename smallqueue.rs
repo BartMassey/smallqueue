4a3ad359f755fd793d47f536cfbfced5d171a236
Bart Massey <bart.massey@gmail.com>
2023-03-28 14:27:10 -0700

    documented

--- smallqueue.rs
+++ smallqueue.rs
@@ -1,3 +1,29 @@
+/*!
+An implementation of a simple queue (first-in first-out)
+data structure that uses no heap storage. As such, this
+queue can be used in `no_std` programs, and may be more
+efficient that [std::collections::VecDeque] in some
+situations.
+
+The queue capacity is specified at compile time using a
+"const generic" value. Internally, the queue is implemented
+using an array with a start index and a length.
+
+This data structure is not inherently thread-safe.
+
+# Examples
+
+```
+# use smallqueue::Queue;
+let mut q: Queue<3, usize> = Queue::default();
+q.insert(17);
+q.insert(18);
+assert_eq!(17, q.extract().unwrap());
+assert_eq!(18, q.extract().unwrap());
+assert!(q.is_empty());
+```
+*/
+
 use core::{
     array,
     mem::MaybeUninit,
@@ -6,12 +32,17 @@ use core::{
 
 use thiserror::Error;
 
+/// Queue errors.
 #[derive(Debug, Error)]
 pub enum QueueError {
+    /// An attempt was made to insert a value into a queue
+    /// that was already full.
     #[error("queue capacity exceeded")]
     Overflow,
 }
 
+/// A queue (first-in first-out) data structure of fixed
+/// capacity, using no heap storage.
 pub struct Queue<const C: usize, T> {
     values: [MaybeUninit<T>; C],
     start: usize,
@@ -19,7 +50,14 @@ pub struct Queue<const C: usize, T> {
 }
 
 impl<const C: usize, T> Queue<C, T> {
-    pub fn insert(&mut self, val: T) -> Result<(), QueueError> {
+    /// Insert the given `value` into the queue.
+    ///
+    /// See the module documentation for an example.
+    ///
+    /// # Errors
+    ///
+    /// Returns [QueueError::Overflow] if the queue is full.
+    pub fn insert(&mut self, value: T) -> Result<(), QueueError> {
         let cap = self.values.len();
         if self.len + 1 > cap {
             return Err(QueueError::Overflow);
@@ -27,12 +65,16 @@ impl<const C: usize, T> Queue<C, T> {
         // Safety: We are only writing to a location at an index that
         // is bounds-checked.
         unsafe {
-            *self.values[(self.start + self.len) % cap].as_mut_ptr() = val;
+            *self.values[(self.start + self.len) % cap].as_mut_ptr() = value;
         }
         self.len += 1;
         Ok(())
     }
 
+    /// Returns `Some` first value in the queue if one
+    /// exists, and `None` otherwise.
+    ///
+    /// See the module documentation for an example.
     pub fn extract(&mut self) -> Option<T> {
         if self.len == 0 {
             return None;
@@ -48,14 +90,48 @@ impl<const C: usize, T> Queue<C, T> {
         Some(val)
     }
 
-    pub fn capacity(&self) -> usize {
+    /// Returns the capacity of this queue (maximum number
+    /// of values that may be stored) as defined at
+    /// compile-time.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use smallqueue::Queue;
+    /// let mut q: Queue<3, usize> = Queue::default();
+    /// assert_eq!(3, q.capacity());
+    /// ```
+    pub const fn capacity(&self) -> usize {
         self.values.len()
     }
 
+    /// Returns the number of values currently stored in the
+    /// queue.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use smallqueue::Queue;
+    /// let mut q: Queue<3, usize> = Queue::default();
+    /// q.insert(17);
+    /// assert_eq!(1, q.len());
+    /// ```
     pub fn len(&self) -> usize {
         self.len
     }
 
+    /// Returns `false` if the queue contains values, but
+    /// `true` otherwise.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use smallqueue::Queue;
+    /// let mut q: Queue<3, usize> = Queue::default();
+    /// assert!(q.is_empty());
+    /// q.insert(17);
+    /// assert!(!q.is_empty());
+    /// ```
     pub fn is_empty(&self) -> bool {
         self.len == 0
     }
